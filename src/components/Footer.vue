158ca0bf62d6a1a9a890f5b172aec05d33ed6bd0
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2022-01-08 23:12:42 -0500

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="py-8 text-center text-white bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500">
     Created by
     <a class="underline" href="https://github.com/uvacoder" target="_blank" rel="noreferrer">
-      jordihales
+      uvacoder
     </a>
     using
     <a class="underline" href="https://tailwindcss.com/" target="_blank" rel="noreferrer">Tailwind</a>
